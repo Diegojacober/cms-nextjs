fceb1a745ef54881aaf745b3a31fa6f26200aeaa
Diego Alencar Jacober <57761933898>
2023-09-12 16:42:58 -0300

    Configuring _document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+                    <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+                </Head>
+                <body>
+                    <Main/>
+                    <NextScript/>
+                </body>
+            </Html>
+        )
+    }
+}
